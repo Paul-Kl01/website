ed6cec7dd30f82b3193490883be8173f1ac4ce55
Paul-Kl01 <kluge.paul@gmx.de>
2020-10-18 16:38:59 +0200

    Member Data fetched

--- components/TeamMember.vue
+++ components/TeamMember.vue
@@ -1,11 +1,9 @@
 <template>
   <div class="member">
-    <div class="image">
-      <img alt="" :src="'https://github.com/' + githubName + '.png?size=44'">
-    </div>
-    <div class="info">
-      <span>{{ name }}{{ role ? ' - ' + role : '' }}</span>
-      <a :href="'https://github.com/' + githubName">Github</a>
+    <div id="java">
+      <div class="name">
+        {{ product.name }}
+      </div>
     </div>
   </div>
 </template>
@@ -14,34 +12,35 @@
   export default {
     name: "TeamMember",
     props: {
-      name: String,
-      role: String,
-      githubName: String
+      product: {
+        type: Object,
+        default: () => {}
+      }
     }
   };
 </script>
 
 <style lang="scss" scoped>
-  div.member {
-    display: flex;
-    flex-direction: row;
-    padding-bottom: 10px;
+div.member {
+  display: flex;
+  flex-direction: row;
+  padding-bottom: 10px;
 
-    div.image {
-      margin-right: 10px;
-      width: 44px;
-      height: 44px;
+  div.image {
+    margin-right: 10px;
+    width: 44px;
+    height: 44px;
 
-      img {
-        border-radius: 50%;
-        width: 100%;
-        height: 100%;
-      }
-    }
-
-    div.info {
-      display: flex;
-      flex-direction: column;
+    img {
+      border-radius: 50%;
+      width: 100%;
+      height: 100%;
     }
   }
+
+  div.info {
+    display: flex;
+    flex-direction: column;
+  }
+}
 </style>
--- pages/team.vue
+++ pages/team.vue
@@ -2,91 +2,12 @@
   <main class="team-page">
     <KNavigationBar title="Team"/>
     <div class="content formatted">
-      <h1>Team</h1>
-
-      <div class="group">
-        <h6>Project Management</h6>
-        <TeamMember github-name="TheMorpheus407" name="Morpheus" role="Community Manager"></TeamMember>
-        <TeamMember github-name="DevMimas" name="Marius" role="Project Manager"></TeamMember>
-        <TeamMember github-name="Chaozz66" name="Chaozz" role="Community Manager"></TeamMember>
-      </div>
-
-      <div class="group">
-        <h6>Backend & Frontend</h6>
-        <TeamMember github-name="Defelo" name="Defelo" role="Head of Development"></TeamMember>
-      </div>
-
-      <div class="group">
-        <h6>Backend</h6>
-        <TeamMember github-name="TheCataliasTNT2k" name="TNT2k" role="Head of Python Backend"></TeamMember>
-        <TeamMember github-name="JannikEmmerich" name="Jannik" role="Head of Java Backend"></TeamMember>
-        <TeamMember github-name="MarcelCoding" name="Marcel" role="Head Assistant of Java Backend"></TeamMember>
-      </div>
-
-      <div class="group">
-        <h6>Frontend</h6>
-        <TeamMember github-name="MaxiHuHe04" name="MaxiHuHe04" role="Head of Frontend"></TeamMember>
-        <TeamMember github-name="JannikEmmerich" name="Jannik" role="Contributor"></TeamMember>
-      </div>
-
-      <div class="group">
-        <h6>Gamedesign</h6>
-        <TeamMember github-name="DevMimas" name="Marius" role="Head of Gamedesign"></TeamMember>
-        <TeamMember github-name="Maulwurf852" name="Maulwurf" role="Head Assistant"></TeamMember>
-        <TeamMember github-name="Tristan-H11" name="Tristan-H11" role="Head Assistant"></TeamMember>
-        <TeamMember github-name="Chaozz66" name="Chaozz" role="Contributor"></TeamMember>
-        <TeamMember github-name="Dev-Fabian" name="Fabian" role="Contributor"></TeamMember>
-        <TeamMember github-name="T2brozz" name="T2brozz" role="Contributor"></TeamMember>
-      </div>
-
-      <div class="group">
-        <h6>Design</h6>
-        <TeamMember github-name="Chaozz66" name="Chaozz" role="Head of Design"></TeamMember>
-        <TeamMember github-name="Nocuma" name="Lars" role="Contributor"></TeamMember>
-        <TeamMember github-name="DevMimas" name="Marius" role="Contributor"></TeamMember>
-      </div>
-
-      <div class="group">
-        <h6>Videoproducer</h6>
-        <TeamMember github-name="Nocuma" name="Lars" role="Head of Videoproduction"></TeamMember>
-      </div>
-
-      <div class="group">
-        <h6>Sounddesign</h6>
-        <TeamMember github-name="Captches" name="Captches" role="Head of Sounddesign"></TeamMember>
-      </div>
-
-      <div class="group">
-        <h6>Editorial office & Translation</h6>
-        <TeamMember github-name="andyRonck" name="Aronck" role="Head of Editorial office & Translation"></TeamMember>
-        <TeamMember github-name="cephox" name="ce_phox " role="Contributor"></TeamMember>
-        <TeamMember github-name="DevMimas" name="Marius " role="Contributor"></TeamMember>
-      </div>
-
-      <div class="group">
-        <h6>Website</h6>
-      </div>
-
-      <div class="group">
-        <h6>Security</h6>
-        <TeamMember github-name="iiestIT" name="iiest_TF" role="Head of Security"></TeamMember>
-      </div>
-
-      <div class="group">
-        <h6>Server Admin</h6>
-        <TeamMember github-name="kurzdev" name="Jokur" role="Admin"></TeamMember>
-        <TeamMember github-name="LordRazor" name="LordRazor" role="Admin"></TeamMember>
-        <TeamMember github-name="felbinger" name="NicoF2000" role="Admin"></TeamMember>
-      </div>
-
-      <div class="group">
-        <h6>Inactive Members</h6>
-        <TeamMember github-name="FelixRewer" name="Felix" role="ex-Head of Frontend"></TeamMember>
-        <TeamMember github-name="SpartanerSpaten" name="EinSpaten" role="ex-Head of Backend"></TeamMember>
-        <TeamMember github-name="mieasy" name="Mieasy" role="ex-Head of Backend"></TeamMember>
-        <TeamMember github-name="TheProgrammer21" name="TheProgrammer" role="ex-Head of Administration"></TeamMember>
-        <TeamMember github-name="RiseofRice" name="RiseofRice" role="ex-Head of Website"></TeamMember>
-        <TeamMember github-name="moritzruth" name="Moritz" role="ex-Website-Contributor"></TeamMember>
+      <div id="java">
+        <TeamMember
+          v-for="member in members"
+          :key="member.name"
+          :product="member"
+        />
       </div>
     </div>
   </main>
@@ -98,27 +19,37 @@ div.content {
   flex-direction: column;
   margin-bottom: 5rem;
 
-  div.group {
+  /*div.group {
     display: flex;
     flex-direction: column;
-  }
+  }*/
 }
 </style>
 
 <script>
-import KNavigationBar from "@/components/KNavigationBar";
-import TeamMember from "../components/TeamMember";
+  import KNavigationBar from "@/components/KNavigationBar";
+  import TeamMember from "../components/TeamMember";
 
-export default {
-  name: "TeamPage",
-  components: {
-    TeamMember,
-    KNavigationBar
-  },
-  head() {
-    return {
-      titleTemplate: "Team - %s"
-    };
-  }
-};
+  export default {
+    name: "TeamPage",
+    components: {
+      TeamMember,
+      KNavigationBar
+    },
+    async fetch() {
+      this.members = await fetch(
+        "https://admin-panel.cryptic.rubidium.ml/api/team/member/list"
+      ).then(res => res.json());
+    },
+    data() {
+      return {
+        members: []
+      };
+    },
+    head() {
+      return {
+        titleTemplate: "Team - %s"
+      };
+    }
+  };
 </script>
